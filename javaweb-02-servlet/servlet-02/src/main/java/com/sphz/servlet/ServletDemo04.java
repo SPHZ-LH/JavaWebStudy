ec586efb4cba24b61f35afd6b46e023f61fa0e8f
SPHZ_LH <1750897010@qq.com>
2024-02-21 22:57:45 +0800

    Add files via upload

---
+++ javaweb-02-servlet/servlet-02/src/main/java/com/sphz/servlet/ServletDemo04.java
@@ -0,0 +1,23 @@
+package com.sphz.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServletDemo04 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = this.getServletContext();
+        RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher("/sd03");
+        requestDispatcher.forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
